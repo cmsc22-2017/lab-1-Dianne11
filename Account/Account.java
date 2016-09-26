e80f70d93b07700d44d6559f469961186de0446d
Dianne11 <diannedeiparine11@gmail.com>
2016-09-26 21:43:03 +0800

    Create Account.java

---
+++ Account/Account.java
@@ -0,0 +1,55 @@
+
+/*public String toString () {
+        return String.format (" %d, $ %.2f", this.accountNumber, this.balance);
+    }
+*/
+import java.util.*;
+
+public class Account {
+    private int accountNumber;
+    private double balance = 0.0;
+
+    public Account(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+	    this.balance = balance;
+    }
+
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    
+    public void credit(double amount) {
+        this.balance = balance + amount;
+    }
+    
+    public void debit(double amount) {
+        if(balance >= amount) {
+            this.balance = balance - amount;
+        } else {
+            throw new IllegalArgumentException("Amount withdrawn exceeds balance!"); 
+        }
+    }
+    
+    /*@Override
+    public String toString(){//overriding the toString() method  
+        return "A/C no: " + accountNumber + ", Balance = $ " + balance;  
+    }*/
+
+    public String toString(){//overriding the toString() method
+        String formattedDate = String.format("A/C: %d , Balance: $%.2f", accountNumber, balance);
+        return formattedDate;  
+    }    
+
+    public double getBalance() {
+	return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public void setAccount(int accountNumber, double balance) {
+	this.accountNumber = accountNumber;
+	this.balance = balance;
+    }
+}
